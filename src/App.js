bbe1e7e01f7ccbe328b1eeeb8afe0e9d81661e0d
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-09-20 08:58:19 -0700

    fetchind data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import Header from "./components/Header.js";
-
+import React from 'react';
+import Header from './components/Header.js';
+import WelcomePage from './components/WelcomePage';
+import { Route } from 'react-router-dom';
+import CharacterList from './components/CharacterList.js';
+import LocationLists from './components/LocationsList';
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
+
+      <Route exact path='/' component={WelcomePage} />
+      <Route exact path='/charectors' component={CharacterList} />
+      <Route exact path='/location' component={LocationLists} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [char, setchar] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        setchar(res);
+        console.log(res);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
   }, []);
 
   return (
-    <section className="character-list">
+    <section className='character-list'>
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
   );
